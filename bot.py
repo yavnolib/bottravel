9a7ed8ddf8c661c870693b60c99d0f1b33b29fa6
yaroslavnikolaevv <51954484+yaroslavnikolaevv@users.noreply.github.com>
2019-11-22 17:11:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,8 @@
 import telebot
 import random
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
 import time, re
-import winsound
-import pyautogui
 import time
 import pyowm
-import pickle
 import requests
 from mqtt import *
 import datetime
