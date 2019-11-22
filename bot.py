3620ec2bdf9d55820f3be941daf3771ee94a5969
yaroslavnikolaevv <51954484+yaroslavnikolaevv@users.noreply.github.com>
2019-11-22 17:39:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -118,7 +118,7 @@ def help_message(message):
 @bot.message_handler(commands=['music'])
 def music(message):
     for i in range(3):
-        n=random.randint(1,5)
+        n=random.randint(1,2)
         audio = open(str(n)+".mp3", mode='rb')
         print("opened "+str(n)+".mp3")
         bot.send_audio(message.from_user.id,audio, timeout=1000)  
