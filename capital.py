d835ea7179c66e6823d0f37cdbfeea54df8ce81b
yaroslavnikolaevv <51954484+yaroslavnikolaevv@users.noreply.github.com>
2019-11-22 20:21:19 +0300

    Add files via upload

---
+++ capital.py
@@ -0,0 +1,10 @@
+class Search_capital:
+	def __init__(self,search):
+        self.searchIn=search
+	def search(self):	
+		a=self.searchIn
+		file=open(r'C:\Users\User\Desktop\test\templates\base_date_city.txt','r',encoding='UTF-8')
+		for line in file:
+			if a in line:
+			    capital=line[0:line.index(':')]
+			    return(capital)
